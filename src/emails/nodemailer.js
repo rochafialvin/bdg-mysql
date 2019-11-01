527376632df0bffa1c840d55833053ecab25bc3b
rochafialvin <rochafi.dev@gmail.com>
2019-11-01 10:44:19 +0700

    Kirim email

---
+++ src/emails/nodemailer.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer')
+const efig = require('./config')
+
+let transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth: {
+        type: 'OAuth2',
+        user: 'rochafi.teach@gmail.com',
+        clientId: efig.clientId,
+        clientSecret: efig.clientSecret,
+        refreshToken: efig.refreshToken
+    }
+})
+
+let mail = {
+    from: 'Rochafi Teach <rochafi.teach@gmail.com>',
+    to: 'rochafi.dev@gmail.com',
+    subject: 'Selamat Datang',
+    html:`<h1>Hello Gengs</h1>`
+}
+
+transporter.sendMail(mail, (err, result) => {
+    if(err) return console.log(err)
+
+    console.log('Email berhasil di kirim')
+})
