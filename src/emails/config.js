e33ba37347937a4e2fa300c59499ba13a4cfd942
Alvin Rochafi <rochafi.dev@gmail.com>
2019-11-01 10:50:42 +0700

    Delete config.js

--- src/emails/config.js
+++
@@ -1,7 +0,0 @@
-let efig = {
-    clientId: '341178108213-3t83gde8r6ia0cv0i0bct9d0v7chi64v.apps.googleusercontent.com',
-    clientSecret: 'B-6oErQlvDBXkV463y9OC91n',
-    refreshToken: '1//04DCvxvAWOf6hCgYIARAAGAQSNwF-L9IrgPOyFXs5ccWhUIKUxaLX9dvPYrrpn_0T3120lKRLO0EjVoLNJS1JIKWE97323THrNfo'
-}
-
-module.exports = efig
